57dc5c095bfeae6946bef2e40dfe7d020d78ddf5
Ice713 <icramzilruiz@gmail.com>
2025-11-08 19:11:09 +0800

    Add Vehicle class with MaxSpeed property
    Added a new file reference for `Vehicle.cs` in the project file `4. Transportation System.csproj`. Introduced a new class `Vehicle` in `Vehicle.cs`, which includes a property `MaxSpeed` of type `decimal`, a constructor to initialize `MaxSpeed`, and a virtual method `ShowDetails` that returns a string representation of the vehicle's maximum speed.

---
+++ 4. Transportation System/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Transportation_System
+{
+    internal class Vehicle
+    {
+        public decimal MaxSpeed { get; set; }
+
+        public Vehicle(decimal maxSpeed)
+        {
+            MaxSpeed = maxSpeed;
+        }
+
+        public virtual string ShowDetails()
+        {
+            return $"Vehicle Max Speed: {MaxSpeed} km/h";
+        }
+    }
+}
