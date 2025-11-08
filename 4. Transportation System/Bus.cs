f0ca0444e9bfed9386e220442265f8983073b2fb
Ice713 <icramzilruiz@gmail.com>
2025-11-08 19:12:11 +0800

    Add Bus class to transportation system project
    Updated project file to include Bus.cs.
    Created Bus class inheriting from Vehicle with properties for SeatingCapacity and methods to display details.

---
+++ 4. Transportation System/Bus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Transportation_System
+{
+    internal class Bus : Vehicle
+    {
+        public int SeatingCapacity { get; set; }
+
+        public Bus(decimal maxSpeed, int seatingCapacity) : base(maxSpeed)
+        {
+            SeatingCapacity = seatingCapacity;
+        }
+
+        public override string ShowDetails()
+        {
+            return $"Bus Max Speed: {MaxSpeed} km/h\nSeating Capacity: {SeatingCapacity} passengers";
+        }
+    }
+}
